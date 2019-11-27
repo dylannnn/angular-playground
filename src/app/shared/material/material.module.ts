0ac3c42acd69db623860f6bb3f9f78eb263b908c
Li, Yunfei (SPLXE) - KLM <Yunfei.Li@klm.com>
2019-11-27 11:45:40 +0100

    add angular material module

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonToggleModule
+  ],
+  exports: [
+    MatButtonToggleModule
+  ]
+})
+export class MaterialModule { }
